0811554059fcfb9348b7b3208f5008a238b9e576
Marco Nikander <m4n1k.developer@gmail.com>
2024-06-13 18:04:18 +0200

    greatly simplified implementation via lambdas and by only allowing the binding of one argument of a binary function

--- test/test_bind.cpp
+++ test/test_bind.cpp
@@ -6,23 +6,16 @@
 
 namespace {
 
-TEST(bind_right, verbose_example)
-{    
-    bd::bind_right<std::plus<int>, int> plusTwo(std::plus<int>{}, 2);
-    const int result = plusTwo(5);
-    EXPECT_EQ(result, 7);
-}
-
 TEST(bind_right, expicit_operator_example)
 {    
-    auto plusTwo     = bd::operator<<=(std::plus<int>{}, 2);
+    auto plusTwo     = bind::operator<<=(std::plus<int>{}, 2);
     const int result = plusTwo(5);
     EXPECT_EQ(result, 7);
 }
 
 TEST(bind_right, plus)
 {
-    using namespace bd;
+    using namespace bind;
     
     auto plusTwo     = std::plus<int>{} <<= 2;
     const int result = plusTwo(5);
@@ -31,21 +24,21 @@ TEST(bind_right, plus)
 
 TEST(bind_right, minus)
 {
-    using namespace bd;
+    using namespace bind;
     
     auto minusTwo    = std::minus<int>{} <<= 2;
     const int result = minusTwo(5);
     EXPECT_EQ(result, 3);
 }
 
-TEST(bind, lambda)
+TEST(bind_right, lambda)
 {
     // Since the function std::pow has many overloads, for float and double for example, it's not as easy to bind its
     // arguments, since the compiler needs to know _which_ overload we are trying to bind to, but overload resolution
     // of a function is only done when we actually call it.
     // For this example we create a lambda which refers explicitely to std::pow(float, float) to showcase that binding
     // works for non-generic lambdas.
-    using namespace bd;
+    using namespace bind;
 
     auto pow = [](int l, int r){ return std::pow(static_cast<float>(l), static_cast<float>(r)); };
     auto square = pow <<= 2;
@@ -55,7 +48,7 @@ TEST(bind, lambda)
 
 TEST(bind_left, plus)
 {
-    using namespace bd;
+    using namespace bind;
 
     auto twoPlus     = 2 >>= std::plus<int>{};
     const int result = twoPlus(5);
@@ -64,7 +57,7 @@ TEST(bind_left, plus)
 
 TEST(bind_left, minus)
 {
-    using namespace bd;
+    using namespace bind;
 
     auto twoMinus    = 2 >>= std::minus<int>{};
     const int result = twoMinus(5);
